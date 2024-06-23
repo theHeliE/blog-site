8bbac45c0ecfc80202dc701257384540c1ba3670
theHeliE <129796088+theHeliE@users.noreply.github.com>
2024-06-23 21:59:32 +0300

    title change

--- app/layout.js
+++ app/layout.js
@@ -6,7 +6,7 @@ import ThemeProvider from "./context/ThemeContext";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "iBorogu - ブログ",
+  title: "iBlog - ブログ",
   description: "Created by TheHazem",
 };
 
